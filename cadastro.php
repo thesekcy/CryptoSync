2cd0cba0957740243d5c53d95a104b4bb8d15596
Fábio Santana <fabio27336f@gmail.com>
2018-10-23 16:56:42 -0300

    Pagina de cadastro usuário
    Este projeto está desatualizado está sendo utilizado para testes, o padrão está nas branchs.

---
+++ cadastro.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CryptoSync - Site Oficial!</title>
+  <!-- Required meta tags -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="\CryptoSync\node_modules\bootstrap\compiler\bootstrap.css">
+  <link rel="stylesheet" href="\CryptoSync\node_modules\bootstrap\compiler\cadastro.css">
+  <!-- Icon Awesome-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+</head>
+<body>
+  <div class="content-wrapper">
+    <div class="content">
+      <div class="signup-wrapper shadow-box">
+        <div class="company-details ">
+          <div class="shadow"></div>
+          <div class="wrapper-1">
+            <div class="logo">
+              <div class="icon-food">
+              </div>
+            </div>
+            <h1 class="title">Plataforma Distribuida</h1>
+            <div class="slogan">Crie sua conta e faça parte do novo mundo!</div>
+          </div>
+        </div>
+        <div class="signup-form ">
+          <div class="wrapper-2">
+            <div class="form-title">Crie sua conta</div>
+            <div class="form">
+              <form>
+                <p class="content-item">
+                  <label>Nome completo
+                    <input type="text" placeholder="Nome"  required>
+                  </label>
+                </p>
+                <p class="content-item">
+                  <label>EMail
+                    <input type="text"  placeholder="email@email.com" name="email" required>
+                  </label>
+                </p>
+                <p class="content-item">
+                  <label>Senha
+                    <input type="password" placeholder="*********" name="senha" required>
+                  </label>
+                </p>
+                <p class="content-item">
+                  <label>repetir Senha
+                    <input type="password" placeholder="*********" name="rsenha" required>
+                  </label>
+                </p>
+                <button type="submit"  class="signup">Criar conta</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</body>
+</html>
+
+
+<!-- 'http://thecodeplayer.com/uploads/media/geometry.png' -->
